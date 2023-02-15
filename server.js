c28534f766c302ad450eff9e1a283977f5817025
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-15 22:04:14 +0100

    connect database

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
 
-app.listen(PORT, async () => {
-	console.log("\nDatabase connection successful.");
-	console.log(`Use our API on port: ${PORT}\n`);
+const connection = mongoose.connect(uriDb, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
+
+connection
+	.then(() => {
+		app.listen(PORT, async () => {
+			console.log("\nDatabase connection successful.");
+			console.log(`Use our API on port: ${PORT}\n`);
+		});
+	})
+	.catch((err) => {
+		console.log(`Server not running. Error message: ${err.message}`);
+		process.exit(1);
+	});
