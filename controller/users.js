788bc6c55d6b6302186ba3e67a7790aa840c54d9
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-16 23:13:18 +0100

    add login endpoint

--- controller/users.js
+++ controller/users.js
@@ -1,18 +1,49 @@
 import gravatar from "gravatar";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import * as service from "../service/users.js";
 import { User } from "../service/schemas/user.js";
+dotenv.config();
+const secret = process.env.SECRET;
 
 export const register = async (req, res, next) => {
 	try {
-		const { email, password } = req.body;
+		const {name, email, password } = req.body;
 		const user = await service.getUser({ email });
 		if (user)
 			return res.status(409).json({ message: "Provided email already exists" });
 		const avatarURL = gravatar.url(email, { s: "60", d: "mp" });
-		const newUser = new User({ email, avatarURL });
+		const newUser = new User({name, email, avatarURL });
 		newUser.setPassword(password);
 		await newUser.save();
-		res.status(201).json({ email, id: newUser._id });
+		res.status(201).json({ email, id: newUser.id });
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const login = async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
+		const user = await service.getUser({ email });
+		if (!user) return res.status(403).json({ message: "Email doesn't exist" });
+		if (!user.validPassword(password))
+			return res.status(403).json({ message: "Password is wrong" });
+
+		const payload = {
+			id: user.id,
+			email: user.email,
+		};
+		const accessToken = jwt.sign(payload, secret, { expiresIn: "1h" });
+		await service.updateUser(user.id, { accessToken });
+		res.status(200).json({
+			accessToken,
+			userData: {
+				email,
+				id: user.id,
+				cards: [],
+			},
+		});
 	} catch (err) {
 		next(err);
 	}
--- utils/validator.js
+++ utils/validator.js
@@ -1,6 +1,7 @@
 import Joi from "joi";
 
 const userSchema = Joi.object({
+	name: Joi.string(),
 	email: Joi.string().email().required(),
 	password: Joi.string().min(8).max(100).regex(/^[a-zA-Z0-9]*$/).required(),
 	token: Joi.string(),
