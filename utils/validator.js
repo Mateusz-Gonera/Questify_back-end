b8cfc5e0741b5fecb6376222940c605d29871647
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-17 10:35:57 +0100

    Update validator.js

--- utils/validator.js
+++ utils/validator.js
@@ -4,7 +4,7 @@ const userSchema = Joi.object({
 	name: Joi.string(),
 	email: Joi.string().email().required(),
 	password: Joi.string().min(8).max(100).regex(/^[a-zA-Z0-9]*$/).required(),
-	token: Joi.string(),
+	accessToken: Joi.string(),
 });
 
 const validator = (schema) => (body) => {
