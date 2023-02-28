fc3cd07acc67b6adf9aac4ef2d3e176db508adde
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-28 20:04:42 +0100

    deploy on Netlify started

--- functions/app.js
+++ functions/app.js
@@ -25,9 +25,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/.netlify/functions/app/auth", usersRouter);
-
-app.use("/.netlify/functions/app/card", cardsRouter);
+app.set("/.netlify/functions/app/auth", usersRouter);
+app.set("/.netlify/functions/app/card", cardsRouter);
 
 app.use((_, res) => {
 	res.status(404).json({
